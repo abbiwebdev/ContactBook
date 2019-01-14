535f506f33037a3f51b06441817c5d343fc1850b
Adan Abbi <Abbiwebdev@gmail.com>
2019-01-14 02:31:39 -0800

    Updates

---
+++ precache-manifest.729009159b0902cf7b52b8d86f11345e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f82510bd1b9acb500e6",
+    "url": "/ContactBook/static/js/runtime~main.0f82510b.js"
+  },
+  {
+    "revision": "7adc193e93c247615c4d",
+    "url": "/ContactBook/static/js/main.7adc193e.chunk.js"
+  },
+  {
+    "revision": "1da4f126796aef1f95b3",
+    "url": "/ContactBook/static/js/1.1da4f126.chunk.js"
+  },
+  {
+    "revision": "7adc193e93c247615c4d",
+    "url": "/ContactBook/static/css/main.a538c83e.chunk.css"
+  },
+  {
+    "revision": "1da4f126796aef1f95b3",
+    "url": "/ContactBook/static/css/1.65ef60c9.chunk.css"
+  },
+  {
+    "revision": "81b0b822d3c8b63790fdbb4a3c84f7ad",
+    "url": "/ContactBook/index.html"
+  }
+];
